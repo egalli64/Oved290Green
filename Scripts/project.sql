98feb1615edcfca57ca832d9c425730d3ccad85e
Chiara Boschiero <boschierochiara@gmail.com>
2020-01-16 16:14:14 +0100

    first commit

---
+++ Scripts/project.sql
@@ -0,0 +1,7 @@
+create database GreenBook;
+create table publishers(
+pub_id integer primary key auto_increment,
+pub_name varchar(20),
+city varchar(20),
+country varchar(20));
+drop table publishers;
---
+++ Scripts/publishers.sql
@@ -0,0 +1,8 @@
+SELECT * FROM greenbook.publishers;
+insert into publishers(pub_name, city, country)
+values ('Feltrinelli', 'Milano', 'IT');
+insert into publishers(pub_name, city, country)
+values ('Giunti', 'Roma', 'IT');
+insert into publishers(pub_name, city, country)
+values ('Mondadori', 'Milano', 'IT');
+commit;
