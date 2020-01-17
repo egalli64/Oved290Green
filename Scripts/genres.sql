86822e7f7cd0793c1c0b46f5004b16791aba8bba
CC <claucada7@gmail.com>
2020-01-17 15:46:10 +0100

    third

--- Scripts/genres.sql
+++ Scripts/genres.sql
@@ -4,3 +4,19 @@ genre_name varchar (20),
 client_id integer references clients(client_id));
 
 commit;
+insert into genres(genre_name)
+values ('saggio');
+
+insert into genres(genre_name)
+values('romanzo');
+
+insert into genres(genre_name)
+values('giallo');
+
+insert into genres(genre_name)
+values('storico');
+
+insert into genres(genre_name)
+values('fantascienza');
+
+commit;
